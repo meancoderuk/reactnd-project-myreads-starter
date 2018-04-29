037ecfde81e5bacef6c105cebc5262932c5d616c
Steven Kearns <steven@meancoder.com>
2018-04-29 08:51:46 +0100

    feat: Create BookshelfList and Bookshelf Components

---
+++ src/components/BookshelfList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Bookshelf from './Bookshelf'
+
+class BookshelList extends Component {
+    render() {
+        return (
+            <div>Bookshelf List</div>
+        )
+    }
+}
+
+export default BookshelList
